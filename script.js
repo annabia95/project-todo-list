14e5c75b58ed6c42c2ae36a44818e8099c3865c1
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-12-15 19:53:31 -0300

    Cumprindo o Requisito 7 (depois de muita luta!)

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const addInput = document.querySelector('#texto-tarefa');
 const addBtn = document.querySelector('#criar-tarefa');
+const listaOrdenada = document.querySelector('#lista-tarefas');
 
-// Requisito 7 - Usei como referência o seguinte vídeo:https://www.youtube.com/watch?v=-BgTrpvOFFc . A monitoria misnistrada pelo Douglas também ajudou na questão de deixar o input vazio após incluir o texto.
+// Requisito 5 e 6 - Usei como referência o seguinte vídeo:https://www.youtube.com/watch?v=-BgTrpvOFFc . A monitoria misnistrada pelo Douglas também ajudou na questão de deixar o input vazio após incluir o texto. Usei uma arrow functions para refatorar o código. link: https://www.w3schools.com/js/js_arrow_function.asp
  addBtn.addEventListener ('click',() => {
     const li = document.createElement ('li');
-    const listaOrdenada = document.querySelector('#lista-tarefas');
+    li.classList = "lista";
+// Atribui a cada elemento da minha lista o valor que foi inserido no input!
     li.innerHTML = addInput.value;
+// "Limpa" o input após o clique!
     addInput.value = '';
     listaOrdenada.appendChild (li); 
+});
+
+// Requisito 7 - Função para criar a cor de fundo de cada item da lista. Tive como referência o Exercício da Agenda Tryber!
+listaOrdenada.addEventListener('click', (event) => {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 });
